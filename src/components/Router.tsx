3983c7922b30c391a0ebf80d3c1c126481c08c47
astronights <shubhankar.a31@gmail.com>
2022-01-30 00:22:30 +0800

    Fixed

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 // import Home from "./Home/Home";
 import About from "./About";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 const Router: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/about" element={<About />} />
-        {/* <Route path="/home" element={About} /> */}
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/about" element={<About />} />
+      {/* <Route path="/home" element={About} /> */}
+    </Routes>
   );
 };
 
